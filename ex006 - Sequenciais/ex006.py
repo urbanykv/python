143cf0cb516a3a3feda28133747c8ad45294efe9
Matheus Urban <matheusurban19@gmail.com>
2023-06-18 15:46:46 -0300

    exercicios

---
+++ ex006 - Sequenciais/ex006.py
@@ -0,0 +1,7 @@
+lista = [101,202,303,404,505]
+print(f"lista = {lista}")
+print(f"lista = {lista[0]}")
+print(f"lista = {lista[1]}")
+print(f"lista = {lista[2]}")
+print(f"lista = {lista[3]}")
+print(f"lista = {lista[4]}")
