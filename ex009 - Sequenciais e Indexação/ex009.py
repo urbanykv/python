7df478867847bd85047efa2b06afe6410636625b
Matheus Urban <matheusurban19@gmail.com>
2023-06-19 13:22:33 -0300

    EX009

--- ex009 - Sequenciais e Indexação/ex009.py
+++ ex009 - Sequenciais e Indexação/ex009.py
@@ -15,10 +15,8 @@ print(f"lista = {lista[3]}")
 print(f"lista = {lista[4]}")
 
 #Lista em colchetes, usando a compreensão de lista.
-lista = [101 for list in iterable]
+
 #Lista usando o construtor do tipo list
-list(iterable)
-print(list)
 
 """Lembrando que assim como em C e em Java, o Python trabalha com a indexaçãodos itens que se inicia com 0 e cada itemtem o seu índice incrementado uma unicade em relação ao seu item anterior. Python tbm permite indexação com números negativos. O valor -1 é o indice do ultimo item, e cada item anterior é decrementado de uma unidade em relação ao sucessor."""
 
