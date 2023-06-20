6382eb05cb3596568214523b8216c5100ae4ca22
Matheus Urban <matheusurban19@gmail.com>
2023-06-20 15:21:23 -0300

    Exercícios

--- Python Estruturado/ex001/ex001 - Emprego de estruturas de decisão e repetição.py
+++ Python Estruturado/ex001/ex001 - Emprego de estruturas de decisão e repetição.py
@@ -6,3 +6,22 @@
 a = 10
 b = 20
 if(a > b):
+    maior = a
+else:
+    maior = b
+print(f"O maior número é: {maior}")
+
+#Estratégia 02
+maior= a
+if(b>maior):
+    maior = b
+print(f"O maior número é: {maior}")
+
+#Exercício 01
+
+x = 2
+if(2 % == 0):
+    print(f"O número é par.")
+else:
+    print("Seu numero é impar.")
+
