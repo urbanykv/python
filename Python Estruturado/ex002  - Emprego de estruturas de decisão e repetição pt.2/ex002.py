3df3cb799db5fe4199e8f65bdc153f94845753a1
Matheus Urban <matheusurban19@gmail.com>
2023-06-20 20:45:24 -0300

    Exercícios

--- Python Estruturado/ex002  - Emprego de estruturas de decisão e repetição pt.2/ex002.py
+++ Python Estruturado/ex002  - Emprego de estruturas de decisão e repetição pt.2/ex002.py
@@ -25,6 +25,9 @@ print("Laço encerrado")
 '''Para ressaltar a diferença entre as instruções break e continue, vamos alterar a linha 3 do código anterior, trocando a instrução continue pela instrução break. Clique em Executar no emulador e veja a nova execução. Essa alteração está no arquivo disponibilizado neste texto (código 9).'''
 
 #A instrução pass
+'''A instrução pass atua sobre a estrutura if, permitindo que ela seja escrita sem outras instruções a serem executadas caso a condição seja verdadeira. Assim, podemos concentrar as instruções no caso em que a condição é falsa.
+
+Suponha que queiramos imprimir somente os números ímpares entre 1 e 10. Uma implementação possível está no emulador seguinte (código 10 do arquivo disponibilizado):'''
 
 for num in range(1, 11):
     if num % 2 == 0:
@@ -32,3 +35,7 @@ for num in range(1, 11):
     else:
         print(num)
 print('Laço encerrado')
+
+'''Claramente, seria possível reescrever a condição do if-else para que pudéssemos transformá-lo em um if simples, sem else. Entretanto, o objetivo aqui é mostrar o uso da instrução pass.
+
+Agora que já vimos os principais conceitos relativos às estruturas de decisão e de repetição, vamos testar seus conhecimentos.'''
