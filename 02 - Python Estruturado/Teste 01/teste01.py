a78f4e2c71fee7fab0d4623b2e8dfa762334d583
Matheus Urban <matheusurban19@gmail.com>
2023-06-21 08:42:00 -0300

    Testes

---
+++ 02 - Python Estruturado/Teste 01/teste01.py
@@ -0,0 +1,18 @@
+#Algoritmo de estrutura de decisão
+
+'''Chupar uma bala
+pegar a bala
+a bala é de cereja?
+    se sim, não chupe a bala
+    se não, continue o algoritmo
+retirar o papel
+chupar a bala
+jogar o papel no lixo'''
+
+bala = input("A bala é de cereja? ")
+if(True):
+    bala = ("sim")
+    print("Que nojo! Odeio bala de cereja.")
+else:
+    bala = ("nao")
+    print("Graças a Deus não é de cereja.")
