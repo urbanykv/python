dda58e38666d17071fd0f095fcf7a16082eaa49c
Matheus Urban <matheusurban19@gmail.com>
2023-07-23 13:54:47 -0300

    py

--- testre.py
+++ testre.py
@@ -1,14 +1,5 @@
-bala = input("oi moça, vc tem bala de banana?")
+a = 5
 
-if(bala == 'sim'):
-    print("vou comprar, pois a bala é de banana")
-elif(bala == 'nao'):
-    print("tem bala de morango então?")
-    if(bala == 'tem'):
-        ('então eu quero de morango msm')
-    elif(bala == 'n tem'):
-        print('infelizmente estamos em falta de bala de banana e morango moço')
-    else:
-        print('infelizmente não conseguimos entender oq vc está querendo dizer')
-else:
-    print('infelizmente não conseguimos entender oq vc está querendo dizer')
+if(a % 2 = 0):
+    print('É par')
+else
